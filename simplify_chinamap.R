a52f23b884c8379212ea555637e9d312a05f472c
Chun-Hui Gao <gaospecial@gmail.com>
2020-02-09 17:02:33 +0800

    Add Province Short Name to China Map Data

--- simplify_chinamap.R
+++ simplify_chinamap.R
@@ -7,7 +7,17 @@ library(stringr)
 
 # chinamap 
 # Thanks to @一棵树 and biobabble for providing county-lvl and recent chinamap data [source: 高德地图]
-chinamap <- read_sf("data-raw/chinamap_county_lvl/chinamap_county_lvl.shp")
+files <- list.files(path = "data-raw/中国_行政边界/",pattern = "shp$",all.files = TRUE,full.names = T,recursive = T)
+
+shapes <- lapply(files, function(file){
+  data <- read_sf(file)
+  provinceName <- gsub(pattern = ".*//",replacement = "",file)
+  provinceName <- gsub(pattern = "_.*",replacement = "",provinceName)
+  data$ProvinceName <- provinceName
+  return(data)
+})
+
+chinamap <- do.call("rbind",shapes)
 
 province_suffix <-  "省|市|自治区|维吾尔自治区|回族自治区|壮族自治区|特别行政区"
 province <- subset(chinamap, Layer=="省") %>%
@@ -23,6 +33,7 @@ saveRDS(province,file = "china_province.RDS")
 # 同样处理市一级水平地图数据
 city_suffix <- "市|城区|回族|藏族|苗族|群岛|布依族|侗族|黎族|地区|林区|蒙古族|朝鲜族|土家族|壮族|傈僳族|哈尼族|彝族|傣族|景颇族|哈萨克|白族|蒙古"
 city <- subset(chinamap, Layer=="市") %>%
-  mutate(name=str_remove_all(Name,pattern = city_suffix))
+  mutate(name=str_remove_all(Name,pattern = city_suffix),
+         provinceShortName=str_remove_all(ProvinceName, pattern = province_suffix))
 city$geometry <- ms_simplify(city$geometry)
 saveRDS(city,file = "china_city.RDS")
