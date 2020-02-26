149fc32812bd066a08d5fa821f09a1b6cd7dd81c
Chun-Hui Gao <gaospecial@gmail.com>
2020-02-26 13:15:13 +0800

    update nCov2019 package

--- simplify_chinamap.R
+++ simplify_chinamap.R
@@ -20,24 +20,21 @@ shapes <- lapply(files, function(file){
 chinamap <- do.call("rbind",shapes)
 
 chinamap$geometry <- ms_simplify(chinamap$geometry)
-write_sf(chinamap,dsn="chinamap_lite.shp")
+# write_sf(chinamap,dsn="chinamap_lite.shp")
 
+# chinamap <- sf::read_sf("inst/chinamap_lite/chinamap_lite.shp")
 
 province_suffix <-  "省|市|自治区|维吾尔自治区|回族自治区|壮族自治区|特别行政区"
 province <- subset(chinamap, Layer=="省") %>%
   mutate(name = str_remove_all(Name, pattern = province_suffix))
-
-# simplify data
-province$geometry <- ms_simplify(province$geometry)
 
 # 保存 province
 saveRDS(province,file = "china_province.RDS")
 
 
 # 同样处理市一级水平地图数据
-city_suffix <- "市|城区|回族|藏族|苗族|群岛|布依族|侗族|黎族|地区|林区|蒙古族|朝鲜族|土家族|壮族|傈僳族|哈尼族|彝族|傣族|景颇族|哈萨克|白族|蒙古"
-city <- subset(chinamap, Layer=="市") %>%
+city_suffix <- "市|区|新区|城区|回族|藏族|苗族|群岛|布依族|侗族|黎族|地区|林区|蒙古族|朝鲜族|土家族|壮族|傈僳族|哈尼族|彝族|傣族|景颇族|哈萨克|白族|蒙古"
+city <- subset(chinamap, Layer=="市" | ( Layer=="区县" & str_detect(ProvinceName,"市") )) %>%
   mutate(name=str_remove_all(Name,pattern = city_suffix),
          provinceShortName=str_remove_all(ProvinceName, pattern = province_suffix))
-city$geometry <- ms_simplify(city$geometry)
 saveRDS(city,file = "china_city.RDS")
